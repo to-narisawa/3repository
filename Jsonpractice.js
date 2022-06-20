008e54269a0d8d34a43248064851d9d62c1c60f6
TouiNarisawa <to-narisawa@japannet.co.jp>
2022-06-20 14:09:14 +0900

    Json

---
+++ Jsonpractice.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+//Jsonデータ
+let member = {
+    "memberId": 1,
+    "membername": "Tanaka Hanako",
+    "age":20
+}
+
+document.writeln("名前:" + member.membername);
+document.writeln("年齢:" + member.age);
